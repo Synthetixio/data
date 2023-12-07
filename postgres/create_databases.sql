765906ac1c4f572ba1c7964634ad3f8397138f83
Troy <troy.burmeister@gmail.com>
2023-12-07 16:50:49 -0700

    set up all live indexers

--- postgres/create_databases.sql
+++ postgres/create_databases.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE base_goerli; -- raw events from Base Goerli
-CREATE DATABASE optimism_goerli; -- raw events from Optimism Goerli
-CREATE DATABASE optimism_mainnet; -- raw events from Optimism Mainnet
-CREATE DATABASE analytics; -- transformed data from dbt
-
+-- transformed data from dbt
+CREATE database analytics;
+-- raw events from Base Goerli
+CREATE database base_goerli;
+-- raw events from Optimism Goerli
+CREATE database optimism_goerli;
+-- raw events from Optimism Mainnet
+CREATE database optimism_mainnet;
 -- create a read only user for querying
-CREATE USER analytics WITH PASSWORD 'analytics';
+CREATE USER analytics WITH password 'analytics';
 GRANT pg_read_all_data TO analytics;
