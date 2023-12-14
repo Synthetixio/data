d0883cece46caca0c27a0975af659da88ae3e3d9
Troy <troy.burmeister@gmail.com>
2023-12-14 12:22:41 -0700

    add base mainnet indexer

--- postgres/create_databases.sql
+++ postgres/create_databases.sql
@@ -1,8 +1,13 @@
-CREATE DATABASE base_goerli; -- raw events from Base Goerli
-CREATE DATABASE optimism_goerli; -- raw events from Optimism Goerli
-CREATE DATABASE optimism_mainnet; -- raw events from Optimism Mainnet
-CREATE DATABASE analytics; -- transformed data from dbt
-
+-- raw events from Optimism Mainnet
+CREATE database optimism_mainnet;
+-- raw events from Optimism Goerli
+CREATE database optimism_goerli;
+-- raw events from Base Goerli
+CREATE database base_mainnet;
+-- raw events from Base Mainnet
+CREATE database base_goerli;
+-- transformed data from dbt
+CREATE database analytics;
 -- create a read only user for querying
-CREATE USER analytics WITH PASSWORD 'analytics';
+CREATE USER analytics WITH password 'analytics';
 GRANT pg_read_all_data TO analytics;
