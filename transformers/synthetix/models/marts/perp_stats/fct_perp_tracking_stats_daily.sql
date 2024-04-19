de573c51b2eec33195dd539cba5908927c4de759
Troy <troy.burmeister@gmail.com>
2024-04-19 11:00:53 -0600

    fix integrators

--- transformers/synthetix/models/marts/perp_stats/fct_perp_tracking_stats_daily.sql
+++ transformers/synthetix/models/marts/perp_stats/fct_perp_tracking_stats_daily.sql
@@ -1,13 +1,15 @@
-WITH stats AS (
+WITH trades AS (
   SELECT
     DATE_TRUNC(
       'day',
       ts
     ) AS ts,
     tracking_code,
-    SUM(trades) AS trades,
-    SUM(fees) AS fees,
-    SUM(volume) AS volume
+    SUM(exchange_fees) AS exchange_fees,
+    SUM(referral_fees) AS referral_fees,
+    SUM(collected_fees) AS collected_fees,
+    SUM(volume) AS volume,
+    SUM(trades) AS trades
   FROM
     {{ ref('fct_perp_tracking_stats_hourly') }}
   GROUP BY
@@ -33,28 +35,34 @@ accounts AS (
 total AS (
   SELECT
     ts,
-    SUM(trades) AS trades_total,
-    SUM(fees) AS fees_total,
-    SUM(volume) AS volume_total
+    SUM(exchange_fees) AS exchange_fees_total,
+    SUM(referral_fees) AS referral_fees_total,
+    SUM(collected_fees) AS collected_fees_total,
+    SUM(volume) AS volume_total,
+    SUM(trades) AS trades_total
   FROM
-    stats
+    trades
   GROUP BY
     1
 )
 SELECT
-  stats.ts,
-  stats.tracking_code,
-  stats.trades,
-  stats.fees,
-  stats.volume,
+  trades.ts,
+  trades.tracking_code,
+  trades.exchange_fees,
+  trades.referral_fees,
+  trades.collected_fees,
+  trades.volume,
+  trades.trades,
   accounts.accounts,
-  stats.fees / total.fees_total AS fees_share,
-  stats.volume / total.volume_total AS volume_share,
-  stats.trades / total.trades_total AS trades_share
+  trades.exchange_fees / total.exchange_fees_total AS exchange_fees_share,
+  trades.referral_fees / total.referral_fees_total AS referral_fees_share,
+  trades.collected_fees / total.collected_fees_total AS collected_fees_share,
+  trades.volume / total.volume_total AS volume_share,
+  trades.trades / total.trades_total AS trades_share
 FROM
-  stats
+  trades
   JOIN accounts
-  ON stats.ts = accounts.ts
-  AND stats.tracking_code = accounts.tracking_code
+  ON trades.ts = accounts.ts
+  AND trades.tracking_code = accounts.tracking_code
   JOIN total
-  ON stats.ts = total.ts
+  ON trades.ts = total.ts
--- transformers/synthetix/models/marts/perp_stats/fct_perp_tracking_stats_hourly.sql
+++ transformers/synthetix/models/marts/perp_stats/fct_perp_tracking_stats_hourly.sql
@@ -5,7 +5,9 @@ WITH trades AS (
       ts
     ) AS ts,
     tracking_code,
-    SUM(total_fees) AS fees,
+    SUM(exchange_fees) AS exchange_fees,
+    SUM(referral_fees) AS referral_fees,
+    SUM(collected_fees) AS collected_fees,
     SUM(notional_trade_size) AS volume,
     SUM(1) AS trades
   FROM
@@ -34,7 +36,9 @@ total AS (
   SELECT
     ts,
     SUM(trades) AS trades_total,
-    SUM(fees) AS fees_total,
+    SUM(exchange_fees) AS exchange_fees_total,
+    SUM(referral_fees) AS referral_fees_total,
+    SUM(collected_fees) AS collected_fees_total,
     SUM(volume) AS volume_total
   FROM
     trades
@@ -44,11 +48,15 @@ total AS (
 SELECT
   trades.ts,
   trades.tracking_code,
-  trades.fees,
+  trades.exchange_fees,
+  trades.referral_fees,
+  trades.collected_fees,
   trades.volume,
   trades.trades,
   accounts.accounts,
-  trades.fees / total.fees_total AS fees_share,
+  trades.exchange_fees / total.exchange_fees_total AS exchange_fees_share,
+  trades.referral_fees / total.referral_fees_total AS referral_fees_share,
+  trades.collected_fees / total.collected_fees_total AS collected_fees_share,
   trades.volume / total.volume_total AS volume_share,
   trades.trades / total.trades_total AS trades_share
 FROM
