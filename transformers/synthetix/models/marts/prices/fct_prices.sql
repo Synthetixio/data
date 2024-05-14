a84b4ffc6064db8123e4113482891783b6ef6981
Troy <troy.burmeister@gmail.com>
2024-05-14 13:24:40 -0600

    fix prices

--- transformers/synthetix/models/marts/prices/fct_prices.sql
+++ transformers/synthetix/models/marts/prices/fct_prices.sql
@@ -1,58 +1,59 @@
-{% if target.name in (
-        'base_mainnet',
-        'base_sepolia'
-    ) %}
-    WITH perp_prices AS (
-        SELECT
-            ts,
-            market_symbol,
-            price
-        FROM
-            {{ ref('fct_perp_market_history') }}
-    ),
-    snx_prices AS (
-        SELECT
-            ts,
-            'SNX' AS market_symbol,
-            snx_price AS price
-        FROM
-            {{ ref('fct_buyback') }}
-        WHERE
-            snx_price > 0
-    ),
-    usdc_prices AS (
-        SELECT
-            ts,
-            'USDC' AS market_symbol,
-            1 AS price
-        FROM
-            {{ ref('core_vault_collateral') }}
+WITH all_prices AS (
+    {% if target.name in (
+            'base_mainnet',
+            'base_sepolia'
+        ) %}
+    SELECT
+        ts,
+        NULL AS market_address,
+        market_symbol,
+        price
+    FROM
+        {{ ref('fct_perp_market_history') }}
+    UNION ALL
+    SELECT
+        ts,
+        NULL AS market_address,
+        'SNX' AS market_symbol,
+        snx_price AS price
+    FROM
+        {{ ref('fct_buyback') }}
+    WHERE
+        snx_price > 0
+    UNION ALL
+    {% endif %}
+    SELECT
+        ts,
+        collateral_type AS market_address,
+        NULL AS market_symbol,
+        amount / collateral_value AS price
+    FROM
+        {{ ref('core_vault_collateral') }}
+    WHERE
+        collateral_value > 0
+),
+tokens AS (
+    SELECT
+        token_address,
+        token_symbol
+    FROM
+        {{ ref(
+            target.name ~ '_tokens'
+        ) }}
+)
+SELECT
+    p.ts,
+    p.market_address,
+    COALESCE(
+        t.token_symbol,
+        p.market_symbol
+    ) AS market_symbol,
+    p.price
+FROM
+    all_prices p
+    LEFT JOIN tokens t
+    ON LOWER(
+        p.market_address
+    ) = LOWER(
+        t.token_address
     )
-SELECT
-    ts,
-    market_symbol,
-    price
-FROM
-    perp_prices
-UNION ALL
-SELECT
-    ts,
-    market_symbol,
-    price
-FROM
-    snx_prices
-UNION ALL
-SELECT
-    ts,
-    market_symbol,
-    price
-FROM
-    usdc_prices
-{% else %}
-SELECT
-    block_timestamp as ts,
-    collateral_type AS market_symbol,
-    deposited_collateral_value / token_amount AS price
-FROM
-    {{ ref('core_market_updated') }}
-{% endif %}
