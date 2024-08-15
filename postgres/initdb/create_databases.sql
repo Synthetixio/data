3b25b8f8bb9c2912a5e5726f5fe059c308573479
marcus-snx <marcus@cc.snxdao.io>
2024-08-15 18:10:45 +0300

    Add agg funcs with ignore nulls

--- postgres/initdb/create_databases.sql
+++ postgres/initdb/create_databases.sql
@@ -13,7 +13,34 @@ CREATE database analytics;
 -- create a read only user for querying
 CREATE USER analytics WITH password 'analytics';
 GRANT pg_read_all_data TO analytics;
+
 -- add the parquet extension
 \c analytics
 CREATE extension IF NOT EXISTS parquet_fdw;
 CREATE server parquet_server foreign DATA wrapper parquet_fdw;
+
+-- Create a function that always returns the first non-NULL value:
+CREATE OR REPLACE FUNCTION first_agg (anyelement, anyelement)
+  RETURNS anyelement
+  LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+'SELECT $1';
+
+-- Then wrap an aggregate around it:
+CREATE AGGREGATE first (anyelement) (
+  SFUNC    = first_agg,
+  STYPE    = anyelement,
+  PARALLEL = safe
+);
+
+-- Create a function that always returns the last non-NULL value:
+CREATE OR REPLACE FUNCTION last_agg (anyelement, anyelement)
+  RETURNS anyelement
+  LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+'SELECT $2';
+
+-- Then wrap an aggregate around it:
+CREATE AGGREGATE last (anyelement) (
+  SFUNC    = last_agg,
+  STYPE    = anyelement,
+  PARALLEL = safe
+);
