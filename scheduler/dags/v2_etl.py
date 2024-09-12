f0cfbd71355553ca679a18fb107f40e25a685bb7
marcus-snx <marcus@cc.snxdao.io>
2024-09-12 23:11:18 +0300

    Update other dags

--- scheduler/dags/v2_etl.py
+++ scheduler/dags/v2_etl.py
@@ -7,6 +7,8 @@ from utils import parse_dbt_output
 
 # environment variables
 WORKING_DIR = os.getenv("WORKING_DIR")
+REPO_URL = os.getenv("REPO_URL")
+REPO_DIR = os.getenv("REPO_CLONE_DIR", "/opt/data")
 
 default_args = {
     "owner": "airflow",
@@ -27,9 +29,26 @@ dag = DAG(
 
 latest_only = LatestOnlyOperator(task_id="latest_only")
 
+sync_repo_optimism_mainnet = BashOperator(
+    task_id="sync_repo_optimism_mainnet",
+    bash_command=f"""
+    if [ -d {REPO_DIR} ]; then
+        cd {REPO_DIR} && git pull && source /home/airflow/venv/bin/activate && dbt deps --project-dir {REPO_DIR}/transformers/synthetix
+    else
+        git clone {REPO_URL} {REPO_DIR} && source /home/airflow/venv/bin/activate && dbt deps --project-dir {REPO_DIR}/transformers/synthetix
+    fi
+    """,
+    env={
+        "WORKING_DIR": WORKING_DIR,
+        "PG_PASSWORD": os.getenv("PG_PASSWORD"),
+    },
+    dag=dag,
+)
+
+
 transform_optimism_mainnet = BashOperator(
     task_id="transform_optimism_mainnet",
-    bash_command=f"source /home/airflow/venv/bin/activate && dbt run --target prod-op --project-dir /opt/synthetix --profiles-dir /opt/synthetix/profiles --profile synthetix",
+    bash_command=f"source /home/airflow/venv/bin/activate && dbt run --target prod-op --project-dir {REPO_DIR}/transformers/synthetix --profiles-dir {REPO_DIR}/transformers/synthetix/profiles --profile synthetix",
     env={
         "WORKING_DIR": WORKING_DIR,
         "PG_PASSWORD": os.getenv("PG_PASSWORD"),
@@ -41,7 +60,7 @@ transform_optimism_mainnet = BashOperator(
 
 test_optimism_mainnet = BashOperator(
     task_id="test_optimism_mainnet",
-    bash_command=f"source /home/airflow/venv/bin/activate && dbt test --target prod-op --project-dir /opt/synthetix --profiles-dir /opt/synthetix/profiles --profile synthetix",
+    bash_command=f"source /home/airflow/venv/bin/activate && dbt test --target prod-op --project-dir {REPO_DIR}/transformers/synthetix --profiles-dir {REPO_DIR}/transformers/synthetix/profiles --profile synthetix",
     env={
         "WORKING_DIR": WORKING_DIR,
         "PG_PASSWORD": os.getenv("PG_PASSWORD"),
@@ -51,4 +70,4 @@ test_optimism_mainnet = BashOperator(
     on_failure_callback=parse_dbt_output,
 )
 
-latest_only >> transform_optimism_mainnet >> test_optimism_mainnet
+latest_only >> sync_repo_optimism_mainnet >> transform_optimism_mainnet >> test_optimism_mainnet
