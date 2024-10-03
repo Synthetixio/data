0f4cb2169172c4051d42ea48f0c9b4adaafcd3c5
marcus-snx <marcus@cc.snxdao.io>
2024-10-03 15:53:20 +0300

    Modify chunk size in squid for parquet partitions

--- indexers/arbitrum-sepolia/src/db.ts
+++ indexers/arbitrum-sepolia/src/db.ts
@@ -5,7 +5,7 @@ import * as tables from './table'
 export let db = new Database({
     tables,
     dest: new LocalDest('/parquet-data-indexers/arbitrum_sepolia_processed'),
-    chunkSizeMb: Infinity,
+    chunkSizeMb: 100,
 })
 
 export type Store = Store_<typeof db extends Database<infer R, any> ? R : never>
