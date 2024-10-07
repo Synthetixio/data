01da7cb50fe0ba77b7e2f071cad5069289e90be8
marcus-snx <marcus@cc.snxdao.io>
2024-10-07 15:56:30 +0300

    Update clickhouse import script

--- clickhouse/clickhouse_import.py
+++ clickhouse/clickhouse_import.py
@@ -1,16 +1,20 @@
 import clickhouse_connect
-from clickhouse_connect.driver.asyncclient import AsyncClient
+from clickhouse_connect.driver.client import Client
 import argparse
 import os
-import asyncio
-import time
+import re
 
-BASE_PATH = "/var/lib/clickhouse/user_files/parquet-data-indexers"
+BASE_PATH = "/var/lib/clickhouse/user_files/parquet-data"
 
 
-def get_event_list(network: str) -> set[str]:
+def convert_case(name):
+    snake_case = re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+    return snake_case
+
+
+def get_event_list(path: str, network: str) -> set[str]:
     event_list = set()
-    for root, dirs, files in os.walk(f"/parquet-data-indexers/{network}"):
+    for root, dirs, files in os.walk(f"{path}"):
         for file in files:
             if file.endswith(".parquet"):
                 event_name = file.split(".")[0]
@@ -19,65 +23,45 @@ def get_event_list(network: str) -> set[str]:
     return event_list
 
 
-async def create_table(
-    client: AsyncClient,
+def create_table(
+    client: Client,
     event_name: str,
     network: str,
+    path: str,
+    hive_partition: bool = False,
 ):
-    print(event_name)
-    print(f"{BASE_PATH}/{network}/*/{event_name}.parquet")
+    table_name = f"{network}.{convert_case(event_name)}"
+    if hive_partition:
+        file_path = f"{path}/*/{event_name}.parquet"
+    else:
+        file_path = f"{path}/{event_name}.parquet"
     query = (
-        f"create table if not exists {event_name}_{network} "
+        f"create table if not exists {table_name} "
         f"engine = MergeTree order by tuple() as "
-        f"select * from file('{BASE_PATH}/{network}/*/{event_name}.parquet')"
+        f"select * from file('{file_path}', 'Parquet')"
     )
-    await client.command(query)
+    client.command(query)
 
 
-async def semaphore_wrapper(
-    client: AsyncClient,
-    sm: asyncio.Semaphore,
-    event_name: str,
-    network: str,
-):
-    print(event_name)
-    print(f"{BASE_PATH}/{network}/*/{event_name}.parquet")
-    async with sm:
-        data = await client.query(
-            f"select * from file('{BASE_PATH}/{network}/*/{event_name}.parquet', 'Parquet')"
-        )
-        await client.insert(
-            f"{event_name}_{network}",
-            data.result_rows,
-            settings={"async_insert": 1, "wait_for_async_insert": 1},
-        )
-
-
-async def main(network: str):
-    client = await clickhouse_connect.get_async_client(
-        host="clickhouse", port=8123, user="default"
-    )
-
-    event_list = get_event_list(network)
-
+def import_data(client: Client, network: str, path: str, hive_partition: bool = False):
+    event_list = get_event_list(path, network)
     for event_name in event_list:
-        await create_table(client, event_name, network)
+        create_table(client, event_name, network, path, hive_partition)
 
-    semaphore = asyncio.Semaphore(4)
 
-    start_time = time.time()
-    await asyncio.gather(
-        *[
-            semaphore_wrapper(client, semaphore, event_name, network)
-            for event_name in event_list
-        ]
-    )
-    end_time = time.time()
-    print(f"Elapsed time: {end_time-start_time:.2f} seconds")
+def main(network: str):
+    client = clickhouse_connect.get_client(host="clickhouse", port=8123, user="default")
+    client.command(f"create database if not exists {network}")
+
+    path_indexed_data = f"{BASE_PATH}/indexed/{network}"
+    path_extracted_data = f"{BASE_PATH}/clean/{network}"
+
+    import_data(client, network, path_indexed_data, hive_partition=True)
+    import_data(client, network, path_extracted_data)
 
 
 if __name__ == "__main__":
-    arg = argparse.ArgumentParser()
-    arg.add_argument("--network", type=str, required=True)
-    args = arg.parse_args()
-    asyncio.run(main(args.network))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--network", type=str, required=True)
+    args = parser.parse_args()
+    main(args.network)
