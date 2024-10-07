3a5038f04cd9665a13974602c039cb8ab09446fd
marcus-snx <marcus@cc.snxdao.io>
2024-10-07 16:07:33 +0300

    Refactor data paths import script

--- clickhouse/clickhouse_import.py
+++ clickhouse/clickhouse_import.py
@@ -4,7 +4,8 @@ import argparse
 import os
 import re
 
-BASE_PATH = "/var/lib/clickhouse/user_files/parquet-data"
+CLICKHOUSE_INTERNAL_PATH = "/var/lib/clickhouse/user_files/parquet-data"
+BASE_PATH = "/parquet-data"
 
 
 def convert_case(name):
@@ -12,9 +13,10 @@ def convert_case(name):
     return snake_case
 
 
-def get_event_list(path: str, network: str) -> set[str]:
+def get_event_list(root: str, network: str) -> set[str]:
+    data_path = f"{BASE_PATH}/{root}/{network}"
     event_list = set()
-    for root, dirs, files in os.walk(f"{path}"):
+    for root, dirs, files in os.walk(f"{data_path}"):
         for file in files:
             if file.endswith(".parquet"):
                 event_name = file.split(".")[0]
@@ -27,14 +29,14 @@ def create_table(
     client: Client,
     event_name: str,
     network: str,
-    path: str,
+    root: str,
     hive_partition: bool = False,
 ):
     table_name = f"{network}.{convert_case(event_name)}"
     if hive_partition:
-        file_path = f"{path}/*/{event_name}.parquet"
+        file_path = f"{CLICKHOUSE_INTERNAL_PATH}/{root}/*/{event_name}.parquet"
     else:
-        file_path = f"{path}/{event_name}.parquet"
+        file_path = f"{CLICKHOUSE_INTERNAL_PATH}/{root}/{event_name}.parquet"
     query = (
         f"create table if not exists {table_name} "
         f"engine = MergeTree order by tuple() as "
@@ -53,11 +55,8 @@ def main(network: str):
     client = clickhouse_connect.get_client(host="clickhouse", port=8123, user="default")
     client.command(f"create database if not exists {network}")
 
-    path_indexed_data = f"{BASE_PATH}/indexed/{network}"
-    path_extracted_data = f"{BASE_PATH}/clean/{network}"
-
-    import_data(client, network, path_indexed_data, hive_partition=True)
-    import_data(client, network, path_extracted_data)
+    import_data(client, network, "indexed", hive_partition=True)
+    import_data(client, network, "clean")
 
 
 if __name__ == "__main__":
