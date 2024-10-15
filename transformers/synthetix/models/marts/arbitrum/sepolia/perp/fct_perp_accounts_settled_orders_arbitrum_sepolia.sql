ef1b0a199df7c50925ab20775fd9672e323bc9c8
marcus-snx <marcus@cc.snxdao.io>
2024-10-15 21:02:22 +0300

    Remove perp accounts settled orders model

--- transformers/synthetix/models/marts/arbitrum/sepolia/perp/fct_perp_accounts_settled_orders_arbitrum_sepolia.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized = "view",
-    tags = ["perp", "account_activity", "arbitrum", "sepolia"]
-) }}
-
-select
-    block_timestamp,
-    account_id
-from {{ ref('perp_order_settled_arbitrum_sepolia') }}
