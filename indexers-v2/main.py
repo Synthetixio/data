412b0d575286db3b9f7175f685cceb866ada046e
marcus-snx <marcus@cc.snxdao.io>
2024-10-25 17:01:21 +0300

    Refactor logic

--- indexers-v2/main.py
+++ indexers-v2/main.py
@@ -6,18 +6,9 @@ import yaml
 from synthetix import Synthetix
 
 
-def get_contract_data(snx, contract_name, package):
-    contract = snx.contracts[package][contract_name]
-    return {
-        "name": contract_name,
-        "address": contract["address"],
-        "abi": contract["abi"],
-    }
-
-
-def save_abi(abi, filename):
+def save_abi(abi, contract_name):
     os.makedirs("abi", exist_ok=True)
-    with open(f"abi/{filename}", "w") as file:
+    with open(f"abi/{contract_name}.json", "w") as file:
         json.dump(abi, file, indent=2)
 
 
@@ -31,15 +22,16 @@ def create_squidgen_config(rpc_url, archive_url, contracts_info):
     }
 
     for contract in contracts_info:
+        name = contract["name"]
+        address = contract["address"]
         contract_config = {
-            "name": contract["name"],
-            "address": contract["address"],
-            "abi": f"./abi/{contract['name']}.json",
+            "name": name,
+            "address": address,
+            "abi": f"./abi/{name}.json",
             "events": True,
             "functions": False,
         }
         config["contracts"].append(contract_config)
-        save_abi(contract["abi"], f"{contract['name']}.json")
 
     return config
 
@@ -77,10 +69,9 @@ def write_yaml(config, filename):
         yaml.dump(config, file, default_flow_style=False)
 
 
-def load_network_config(network_name):
-    with open("networks.yaml", "r") as file:
-        networks = yaml.safe_load(file)
-    return networks.get(network_name)
+def load_network_config(path):
+    with open(f"{path}/network_config.yaml", "r") as file:
+        return yaml.safe_load(file)
 
 
 if __name__ == "__main__":
@@ -91,38 +82,57 @@ if __name__ == "__main__":
     parser.add_argument("--rpc_endpoint", type=str, help="RPC URL", required=True)
     args = parser.parse_args()
 
-    network_config = load_network_config(args.network_name)
+    network_name = args.network_name
+    path = f"networks/{network_name}"
+    network_config = load_network_config(path)[network_name]
 
     if not network_config:
-        print(f"Network '{args.network_name}' not found in networks.yaml")
+        print(f"Network '{network_name}' not found in {path}/network_config.yaml")
         sys.exit(1)
 
-    rpc_endpoint = os.environ.get("RPC_ENDPOINT")
+    rpc_endpoint = args.rpc_endpoint
     if not rpc_endpoint:
         print("RPC_ENDPOINT environment variable is not set")
         sys.exit(1)
     archive_url = network_config["archive_url"]
-    contracts_to_include = network_config["contracts"]
 
-    print(network_config)
-    if "cannon_config" in network_config:
-        snx = Synthetix(
-            provider_rpc=rpc_endpoint,
-            network_id=network_config["network_id"],
-            cannon_config=network_config["cannon_config"],
-        )
-    else:
-        snx = Synthetix(
-            provider_rpc=rpc_endpoint,
-            network_id=network_config["network_id"],
-        )
+    contracts = []
 
-    contracts_info = [
-        get_contract_data(snx, contract_name, package)
-        for contract_name, package in contracts_to_include
-    ]
+    if "contracts_from_sdk" in network_config:
+        contracts_from_sdk = network_config["contracts_from_sdk"]
 
-    squidgen_config = create_squidgen_config(rpc_endpoint, archive_url, contracts_info)
+        if "cannon_config" in network_config:
+            snx = Synthetix(
+                provider_rpc=rpc_endpoint,
+                network_id=network_config["network_id"],
+                cannon_config=network_config["cannon_config"],
+            )
+        else:
+            snx = Synthetix(
+                provider_rpc=rpc_endpoint,
+                network_id=network_config["network_id"],
+            )
+
+        for contract in contracts_from_sdk:
+            name = contract["name"]
+            package = contract["package"]
+            contract_data = snx.contracts[package][name]
+            if name == "buyback_snx":
+                name = "BuybackSnx"
+            save_abi(contract_data["abi"], name)
+            contracts.append({"name": name, "address": contract_data["address"]})
+
+    if "contracts_from_abi" in network_config:
+        contracts_from_abi = network_config["contracts_from_abi"]
+
+        for contract in contracts_from_abi:
+            name = contract["name"]
+            with open(f"{path}/abi/{name}.json", "r") as file:
+                contract_abi = json.load(file)
+            save_abi(contract_abi, name)
+            contracts.append({"name": name, "address": contract["address"]})
+
+    squidgen_config = create_squidgen_config(rpc_endpoint, archive_url, contracts)
     write_yaml(squidgen_config, "squidgen.yaml")
 
     squid_config = create_squid_config(args.network_name)
