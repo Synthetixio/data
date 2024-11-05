3dcf8ce189b89d7361404168c1f93e06036365db
Troy <troy.burmeister@gmail.com>
2024-11-05 13:38:58 -0700

    fix linting errors

--- transformers/synthetix/models/marts/arbitrum/mainnet/perp/fct_perp_collateral_balances_arbitrum_mainnet.sql
+++ transformers/synthetix/models/marts/arbitrum/mainnet/perp/fct_perp_collateral_balances_arbitrum_mainnet.sql
@@ -13,10 +13,8 @@ transfers as (
         cm.transaction_hash,
         cm.collateral_id,
         synths.synth_token_address,
-        account_id,
-        {{ convert_wei(
-            'cm.amount_delta'
-        ) }} as amount_delta
+        cm.account_id,
+        {{ convert_wei('cm.amount_delta') }} as amount_delta
     from
         {{ ref('perp_collateral_modified_arbitrum_mainnet') }} as cm
     inner join synths
@@ -33,12 +31,8 @@ liq_tx as (
 
 distributors as (
     select
-        CAST(
-            rd.distributor_address as text
-        ) as distributor_address,
-        CAST(
-            rd.token_symbol as text
-        ) as token_symbol,
+        CAST(rd.distributor_address as text) as distributor_address,
+        CAST(rd.token_symbol as text) as token_symbol,
         rd.synth_token_address,
         synths.collateral_id
     from
@@ -49,17 +43,17 @@ distributors as (
 
 liquidations as (
     select
-        block_number,
-        block_timestamp as ts,
-        -amount / 1e18 as amount_delta,
+        rd.block_number,
+        rd.block_timestamp as ts,
+        -rd.amount / 1e18 as amount_delta,
         liq_tx.transaction_hash,
-        collateral_type,
-        token_symbol,
-        synth_token_address,
-        account_id,
+        rd.collateral_type,
+        distributors.token_symbol,
+        distributors.synth_token_address,
+        liq_tx.account_id,
         distributors.collateral_id
     from
-        prod_raw_arbitrum_mainnet.core_rewards_distributed_arbitrum_mainnet as rd
+        {{ ref('core_rewards_distributed_arbitrum_mainnet') }} as rd
     inner join liq_tx
         on rd.transaction_hash = liq_tx.transaction_hash
     inner join distributors
@@ -77,71 +71,59 @@ net_transfers as (
         events.account_id,
         prices.price,
         events.amount_delta,
-        SUM(
-            events.amount_delta
-        ) over (
-            partition by
-                events.account_id,
-                events.collateral_id
-            order by
-                events.ts
+        SUM(events.amount_delta) over (
+            partition by events.account_id, events.collateral_id
+            order by events.ts
         ) as account_balance,
-        SUM(
-            events.amount_delta
-        ) over (
+        SUM(events.amount_delta) over (
             partition by events.collateral_id
-            order by
-                events.ts
+            order by events.ts
         ) as total_balance
-    from
-        (
-            select
-                ts,
-                transaction_hash,
-                collateral_id,
-                synth_token_address,
-                account_id,
-                amount_delta,
-                'transfer' as event_type
-            from
-                transfers
-            union all
-            select
-                ts,
-                transaction_hash,
-                collateral_id,
-                synth_token_address,
-                account_id,
-                amount_delta,
-                'liquidation' as event_type
-            from
-                liquidations
-        ) as events
+    from (
+        select
+            transfers.ts,
+            transfers.transaction_hash,
+            transfers.collateral_id,
+            transfers.synth_token_address,
+            transfers.account_id,
+            transfers.amount_delta,
+            'transfer' as event_type
+        from
+            transfers
+        union all
+        select
+            liquidations.ts,
+            liquidations.transaction_hash,
+            liquidations.collateral_id,
+            liquidations.synth_token_address,
+            liquidations.account_id,
+            liquidations.amount_delta,
+            'liquidation' as event_type
+        from
+            liquidations
+    ) as events
     inner join {{ ref('arbitrum_mainnet_synths') }} as synths
         on events.collateral_id = synths.synth_market_id
     left join {{ ref('fct_prices_hourly_arbitrum_mainnet') }} as prices
         on
             synths.token_symbol = prices.market_symbol
-            and DATE_TRUNC(
-                'hour',
-                events.ts
-            ) = prices.ts
+            and DATE_TRUNC('hour', events.ts) = prices.ts
 )
 
 select
-    ts,
-    transaction_hash,
-    event_type,
-    collateral_id,
-    synth_token_address,
-    synth_symbol,
-    account_id,
-    price,
-    amount_delta,
-    amount_delta * price as amount_delta_usd,
-    account_balance,
-    account_balance * price as account_balance_usd,
-    total_balance,
-    total_balance * price as total_balance_usd
+    net_transfers.ts,
+    net_transfers.transaction_hash,
+    net_transfers.event_type,
+    net_transfers.collateral_id,
+    net_transfers.synth_token_address,
+    net_transfers.synth_symbol,
+    net_transfers.account_id,
+    net_transfers.price,
+    net_transfers.amount_delta,
+    net_transfers.amount_delta * net_transfers.price as amount_delta_usd,
+    net_transfers.account_balance,
+    net_transfers.account_balance * net_transfers.price as account_balance_usd,
+    net_transfers.total_balance,
+    net_transfers.total_balance * net_transfers.price as total_balance_usd
 from
     net_transfers
