7a4ab5169da48889200b208de70c8edc49c8dc53
Troy <troy.burmeister@gmail.com>
2024-12-04 15:12:46 -0700

    linting

--- postgres/initdb/create_databases.sql
+++ postgres/initdb/create_databases.sql
@@ -1,50 +1,50 @@
 -- raw events from Ethereum Mainnet
-CREATE database eth_mainnet;
+CREATE DATABASE eth_mainnet;
 -- raw events from Optimism Mainnet Synthetix
-CREATE database optimism_mainnet;
+CREATE DATABASE optimism_mainnet;
 -- raw events from Optimism Mainnet TLX
-CREATE database optimism_mainnet_tlx;
+CREATE DATABASE optimism_mainnet_tlx;
 -- raw events from Base Mainnet
-CREATE database base_mainnet;
+CREATE DATABASE base_mainnet;
 -- raw events from Base Sepolia
-CREATE database base_sepolia;
+CREATE DATABASE base_sepolia;
 -- raw events from Arbitrum Sepolia
-CREATE database arbitrum_sepolia;
+CREATE DATABASE arbitrum_sepolia;
 -- raw events from Arbitrum Mainnet
-CREATE database arbitrum_mainnet;
+CREATE DATABASE arbitrum_mainnet;
 -- transformed data from dbt
-CREATE database analytics;
+CREATE DATABASE analytics;
 -- create a read only user for querying
-CREATE USER analytics WITH password 'analytics';
+CREATE USER analytics WITH PASSWORD 'analytics';
 GRANT pg_read_all_data TO analytics;
 
 -- add the parquet extension
 \c analytics
-CREATE extension IF NOT EXISTS parquet_fdw;
-CREATE server parquet_server foreign DATA wrapper parquet_fdw;
+CREATE EXTENSION IF NOT EXISTS parquet_fdw;
+CREATE SERVER parquet_server FOREIGN DATA WRAPPER parquet_fdw;
 
 -- Create a function that always returns the first non-NULL value:
-CREATE OR REPLACE FUNCTION first_agg (anyelement, anyelement)
-  RETURNS anyelement
-  LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+CREATE OR REPLACE FUNCTION first_agg(anyelement, anyelement)
+RETURNS anyelement
+LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
 'SELECT $1';
 
 -- Then wrap an aggregate around it:
 CREATE AGGREGATE first (anyelement) (
-  SFUNC    = first_agg,
-  STYPE    = anyelement,
-  PARALLEL = safe
+    sfunc = first_agg,
+    stype = anyelement,
+    parallel = safe
 );
 
 -- Create a function that always returns the last non-NULL value:
-CREATE OR REPLACE FUNCTION last_agg (anyelement, anyelement)
-  RETURNS anyelement
-  LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
+CREATE OR REPLACE FUNCTION last_agg(anyelement, anyelement)
+RETURNS anyelement
+LANGUAGE sql IMMUTABLE STRICT PARALLEL SAFE AS
 'SELECT $2';
 
 -- Then wrap an aggregate around it:
 CREATE AGGREGATE last (anyelement) (
-  SFUNC    = last_agg,
-  STYPE    = anyelement,
-  PARALLEL = safe
+    sfunc = last_agg,
+    stype = anyelement,
+    parallel = safe
 );
