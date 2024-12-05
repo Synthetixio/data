f979a933221a65f3571490ef953402bf9ec56f6f
Troy <troy.burmeister@gmail.com>
2024-12-05 14:41:42 -0700

    add tests

--- transformers/synthetix/models/marts/optimism/mainnet/lt_trades_optimism_mainnet.sql
+++ transformers/synthetix/models/marts/optimism/mainnet/lt_trades_optimism_mainnet.sql
@@ -22,7 +22,7 @@ with trades as (
             account,
             token,
             {{ convert_wei(leveraged_token_amount) }} as leveraged_token_amount,
-            {{ convert_wei(base_asset_amount) }} as base_asset_amount,
+            {{ convert_wei(base_asset_amount) }} as base_asset_amount
         from prod_raw_optimism_mainnet.tlx_lt_minted_optimism_mainnet
         union all
         select
@@ -34,23 +34,29 @@ with trades as (
             event_name,
             account,
             token,
-            {{ convert_wei(leveraged_token_amount) }} * -1 as leveraged_token_amount,
-            {{ convert_wei(base_asset_amount) }} * -1 as base_asset_amount,
+            {{ convert_wei(leveraged_token_amount) }}
+            * -1 as leveraged_token_amount,
+            {{ convert_wei(base_asset_amount) }} * -1 as base_asset_amount
         from prod_raw_optimism_mainnet.tlx_lt_redeemed_optimism_mainnet
-    ) a
+    ) as a
 ),
+
 prices as (
     select distinct
         market,
         block_number,
-        last(price) over (partition by market, block_number order by id) as price
+        last(price)
+            over (partition by market, block_number order by id)
+        as price
     from prod_optimism_mainnet.fct_v2_trades_optimism_mainnet
 )
+
 select
     trades.*,
     prices.price,
     abs(trades.leveraged_token_amount) * prices.price as volume
 from trades
 left join prices
-on trades.market = prices.market
-and trades.block_number = prices.block_number
+    on
+        trades.market = prices.market
+        and trades.block_number = prices.block_number
