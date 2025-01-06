fae9ed617f0eb3bfe89e38f80e87cc5dbc52105a
marcus-snx <marcus@cc.snxdao.io>
2025-01-06 20:47:40 +0200

    Add dbt deps and git clone in entrypoint

--- scheduler/entrypoint.sh
+++ scheduler/entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ -z "$REPO_URL" ]; then
+    echo "REPO_URL is not set"
+    exit 1
+fi
+
+REPO_DIR=${REPO_CLONE_DIR:-"/opt/data"}
+
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Cloning repo $REPO_URL into $REPO_DIR"
+    git clone $REPO_URL $REPO_DIR
+
+    echo "Installing dbt dependencies"
+    source /home/airflow/venv/bin/activate
+    dbt deps --project-dir "$REPO_DIR/transformers/synthetix"
+    source deactivate
+fi
+
 airflow db upgrade
 airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l user || true
 airflow webserver --port 8080 &
