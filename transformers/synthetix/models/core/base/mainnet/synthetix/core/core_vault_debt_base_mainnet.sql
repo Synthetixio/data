23f2b1bad42258775f1c9ced2b40b4095f7507e5
marcus-snx <marcus@cc.snxdao.io>
2025-01-29 01:53:15 +0200

    Fix vault_debt field schema

--- transformers/synthetix/models/core/base/mainnet/synthetix/core/core_vault_debt_base_mainnet.sql
+++ transformers/synthetix/models/core/base/mainnet/synthetix/core/core_vault_debt_base_mainnet.sql
@@ -5,14 +5,14 @@ with base as (
         chain_id,
         pool_id,
         collateral_type,
-        output_0 as debt 
+        value_1 as debt 
     from
         {{ source(
             'raw_base_mainnet',
             'synthetix_core_proxy_function_get_vault_debt'
         ) }}
     where
-        output_0 is not null
+        value_1 is not null
 )
 
 select
