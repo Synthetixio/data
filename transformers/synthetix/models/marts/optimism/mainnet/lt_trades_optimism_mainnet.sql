a541f6cd6db1a7d6be1c1c02db38483281f5500a
Troy <troy.burmeister@gmail.com>
2025-02-03 11:51:34 -0700

    update op mainnet model

--- transformers/synthetix/models/marts/optimism/mainnet/lt_trades_optimism_mainnet.sql
+++ transformers/synthetix/models/marts/optimism/mainnet/lt_trades_optimism_mainnet.sql
@@ -1,5 +1,13 @@
-with trades as (
+with
+zap_accounts as (
+    select distinct
+        transaction_hash,
+        account
+    from
+        {{ ref('tlx_lt_zap_swaps_optimism_mainnet') }}
+),
 
+trades as (
     select
         a.id,
         a.block_number,
@@ -13,61 +21,59 @@ with trades as (
         upper(substring(a.token from '([^_]+)')) as market,
         a.leveraged_token_amount,
         a.base_asset_amount,
-        abs(a.leveraged_token_amount) as nominal_volume,
-        abs(a.leveraged_token_amount) * a.leverage as notional_volume
+        abs(a.base_asset_amount) as nominal_volume,
+        abs(a.base_asset_amount) * a.leverage as notional_volume,
+        abs(a.base_asset_amount) / abs(a.leveraged_token_amount) as token_price,
+        sum(a.leveraged_token_amount)
+            over (partition by contract order by ts)
+        as total_supply
     from (
-
         select
-            id,
-            block_number,
-            block_timestamp as ts,
-            transaction_hash,
-            contract,
-            event_name,
-            account,
-            token,
+            m.id,
+            m.block_number,
+            m.block_timestamp as ts,
+            m.transaction_hash,
+            m.contract,
+            m.event_name,
+            COALESCE(z.account, m.account) as account,
+            m.token,
             cast(
-                regexp_replace(token, '.*_(long|short)', '') as int
+                regexp_replace(m.token, '.*_(long|short)', '') as int
             ) as leverage,
-            {{ convert_wei('leveraged_token_amount') }}
+            -- Swapped columns for contract bug
+            {{ convert_wei('m.base_asset_amount') }}
             as leveraged_token_amount,
-            {{ convert_wei('base_asset_amount') }} as base_asset_amount
-        from {{ ref('tlx_lt_minted_optimism_mainnet') }}
+            {{ convert_wei('m.leveraged_token_amount') }} as base_asset_amount
+        from {{ ref('tlx_lt_minted_optimism_mainnet') }} as m
+        left join zap_accounts as z
+            on m.transaction_hash = z.transaction_hash
+        
         union all
+        
         select
-            id,
-            block_number,
-            block_timestamp as ts,
-            transaction_hash,
-            contract,
-            event_name,
-            account,
-            token,
+            r.id,
+            r.block_number,
+            r.block_timestamp as ts,
+            r.transaction_hash,
+            r.contract,
+            r.event_name,
+            COALESCE(z.account, r.account) as account,
+            r.token,
             cast(
-                regexp_replace(token, '.*_(long|short)', '') as int
+                regexp_replace(r.token, '.*_(long|short)', '') as int
             ) as leverage,
-            {{ convert_wei('leveraged_token_amount') }}
+            -- Swapped columns for contract bug
+            {{ convert_wei('r.base_asset_amount') }}
             * -1 as leveraged_token_amount,
-            {{ convert_wei('base_asset_amount') }} * -1 as base_asset_amount
-        from {{ ref('tlx_lt_redeemed_optimism_mainnet') }}
+            {{ convert_wei('r.leveraged_token_amount') }} * -1 as base_asset_amount
+        from {{ ref('tlx_lt_redeemed_optimism_mainnet') }} as r
+        left join zap_accounts as z
+            on r.transaction_hash = z.transaction_hash
     ) as a
-),
-
-prices as (
-    select distinct
-        market,
-        block_number,
-        last(price)
-            over (partition by market, block_number order by id)
-        as price
-    from {{ ref('fct_v2_trades_optimism_mainnet') }}
 )
 
 select
-    trades.*,
-    prices.price
+    *,
+    total_supply * token_price as vault_tvl,
+    total_supply * token_price * leverage as vault_oi
 from trades
-left join prices
-    on
-        trades.market = prices.market
-        and trades.block_number = prices.block_number
